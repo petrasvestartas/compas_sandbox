b5cbc814ce5ad075209ba9f2099563c3a9c9ca3b
Petras Vestartas <petrasvestartas@gmail.com>
2026-08-23 13:25:05 +0200

    Bump version to 0.6.3

--- src/compas_sandbox/__init__.py
+++ src/compas_sandbox/__init__.py
@@ -4,7 +4,7 @@ __author__ = ["Gene Ting-Chun Kao"]
 __copyright__ = "Gene Ting-Chun Kao"
 __license__ = "MIT License"
 __email__ = "kao@arch.ethz.ch, kao.gene@gmail.com"
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 
 HERE = os.path.dirname(__file__)
